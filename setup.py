e1c8b71c2cf3a114cd08dd96e737fe08efc355f2
Brendan Donegan <brendan.j.donegan@gmail.com>
2013-05-08 15:48:06 +0100

    Fix private bugs issue in lp-file-bug and increment version to 0.8 in lp-file-bug

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name = "lp-helpers",
-    version = '0.7',
+    version = '0.8',
     author = "Brendan Donegan",
     author_email = "brendan-donegan@canonical.com",
     license = "GPL",
